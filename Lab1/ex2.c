8eeacb8e83418f5bc36615ccef4b173312774058
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-03 19:34:47 +0200

    Initial commit

---
+++ Lab1/ex2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Wojhkqhkqhkllld!\n");
+    return 0;
+}
---
+++ Lab1/ex3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("llllll\n");
+    return 0;
+}
